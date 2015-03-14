a8f489c5fced555f50e6c5c766331a2c4c8a7774
S. Chris Colbert <sccolbert@gmail.com>
2015-03-14 17:44:49 -0400

    add WidgetFlag enum

---
+++ src/widgets/WidgetFlag.ts
@@ -0,0 +1,42 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2015, S. Chris Colbert
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+export = WidgetFlag;
+
+
+/**
+ * An enum of widget bit flags.
+ *
+ * Widget flags are used to control various low-level behaviors of
+ * a widget. They are typcially not used directly by user code.
+ */
+enum WidgetFlag {
+  /**
+   * The widget is attached to the DOM.
+   */
+  IsAttached = 0x1,
+
+  /**
+   * The widget is explicitly hidden.
+   */
+  IsHidden = 0x2,
+
+  /**
+   * The widget is visible.
+   */
+  IsVisible = 0x4,
+
+  /**
+   * The widget has been disposed.
+   */
+  IsDisposed = 0x8,
+
+  /**
+   * Changing the widget layout is disallowed.
+   */
+  DisallowLayoutChange = 0x10,
+}
