202bf0e665d7b0f7e606dc1269f0b5710149f38f
S. Chris Colbert <sccolbert@gmail.com>
2015-03-14 18:28:40 -0400

    add ResizeEvent class

---
+++ src/widgets/ResizeEvent.ts
@@ -0,0 +1,60 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2015, S. Chris Colbert
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+import CoreEvent = require('../core/CoreEvent');
+
+export = ResizeEvent;
+
+
+/**
+ * An event class for 'resize' events.
+ */
+class ResizeEvent extends CoreEvent {
+  /**
+   * Construct a new resize event.
+   */
+  constructor(oldWidth: number, oldHeight: number, width: number, height: number) {
+    super('resize');
+    this._m_oldWidth = oldWidth;
+    this._m_oldHeight = oldHeight;
+    this._m_width = width;
+    this._m_height = height;
+  }
+
+  /**
+   * The old width of the widget.
+   */
+  get oldWidth(): number {
+    return this._m_oldWidth;
+  }
+
+  /**
+   * The old height of the widget.
+   */
+  get oldHeight(): number {
+    return this._m_oldHeight;
+  }
+
+  /**
+   * The current width of the widget.
+   */
+  get width(): number {
+    return this._m_width;
+  }
+
+  /**
+   * The current height of the widget.
+   */
+  get height(): number {
+    return this._m_height;
+  }
+
+  private _m_oldWidth: number;
+  private _m_oldHeight: number;
+  private _m_width: number;
+  private _m_height: number;
+}
