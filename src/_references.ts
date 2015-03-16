54feb27fdccd6c7582103f86ce562abe726f031e
S. Chris Colbert <sccolbert@gmail.com>
2015-03-16 02:32:09 -0400

    add a reference file for proper output ordering

---
+++ src/_references.ts
@@ -0,0 +1,49 @@
+/// <reference path="collections/IIterator.ts" />
+/// <reference path="collections/IIterable.ts" />
+/// <reference path="collections/ICollection.ts" />
+/// <reference path="collections/IList.ts" />
+/// <reference path="collections/IQueue.ts" />
+/// <reference path="collections/ReadOnlyCollection.ts" />
+/// <reference path="collections/ReadOnlyList.ts" />
+/// <reference path="collections/ListIterator.ts" />
+/// <reference path="collections/ArrayList.ts" />
+/// <reference path="collections/CircularBuffer.ts" />
+/// <reference path="collections/Queue.ts" />
+
+/// <reference path="core/ICoreEvent.ts" />
+/// <reference path="core/IDisposable.ts" />
+/// <reference path="core/IEventFilter.ts" />
+/// <reference path="core/IEventHandler.ts" />
+/// <reference path="core/algorithm.ts" />
+/// <reference path="core/CoreEvent.ts" />
+/// <reference path="core/Disposable.ts" />
+/// <reference path="core/eventloop.ts" />
+/// <reference path="core/Signal.ts" />
+
+/// <reference path="di/IInjectable.ts" />
+/// <reference path="di/Container.ts" />
+
+/// <reference path="dom/boxdata.ts" />
+/// <reference path="dom/overrideCursor.ts" />
+
+/// <reference path="geometry/Point.ts" />
+/// <reference path="geometry/Size.ts" />
+/// <reference path="geometry/Rect.ts" />
+
+/// <reference path="widgets/ILayout.ts" />
+/// <reference path="widgets/ILayoutItem.ts" />
+/// <reference path="widgets/Alignment.ts" />
+/// <reference path="widgets/Orientation.ts" />
+/// <reference path="widgets/SizePolicy.ts" />
+/// <reference path="widgets/WidgetFlag.ts" />
+/// <reference path="widgets/ChildEvent.ts" />
+/// <reference path="widgets/MoveEvent.ts" />
+/// <reference path="widgets/ResizeEvent.ts" />
+/// <reference path="widgets/SizingItem.ts" />
+/// <reference path="widgets/layoutCalc.ts" />
+/// <reference path="widgets/WidgetItem.ts" />
+/// <reference path="widgets/Layout.ts" />
+/// <reference path="widgets/Widget.ts" />
+/// <reference path="widgets/SplitterHandle.ts" />
+/// <reference path="widgets/SplitterLayout.ts" />
+/// <reference path="widgets/Splitter.ts" />
