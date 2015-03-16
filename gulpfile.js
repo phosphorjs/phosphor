ce63ecc43121848d4549bf28e56248998ab0ab3c
Chris Colbert <sccolbert@gmail.com>
2015-03-16 10:38:02 -0400

    order the files during build instead of using reference paths

--- gulpfile.js
+++ gulpfile.js
@@ -17,50 +17,94 @@ var stylus = require('gulp-stylus');
 var typescript = require('gulp-typescript');
 
 
-var sourceDir = './src/';
-var stylDir = './styl/';
-var distDir = './dist/';
-var buildDir = './build/';
-var jsBuildDir = buildDir + 'js/';
-var dtsBuildDir = buildDir + 'dts/';
-var cssBuildDir = buildDir + 'css/';
+var tsSources = [
+  'collections/IIterator',
+  'collections/IIterable',
+  'collections/ICollection',
+  'collections/IList',
+  'collections/IQueue',
+  'collections/ReadOnlyCollection',
+  'collections/ReadOnlyList',
+  'collections/ListIterator',
+  'collections/ArrayList',
+  'collections/CircularBuffer',
+  'collections/Queue',
 
-var tsSources = sourceDir + '**/*.ts';
-var jsSources = jsBuildDir + '**/*.js';
-var stylSources = stylDir + 'index.styl';
+  'core/ICoreEvent',
+  'core/IDisposable',
+  'core/IEventFilter',
+  'core/IEventHandler',
+  'core/algorithm',
+  'core/CoreEvent',
+  'core/Disposable',
+  'core/eventloop',
+  'core/Signal',
+
+  'di/IInjectable',
+  'di/Container',
+
+  'dom/boxdata',
+  'dom/overrideCursor',
+
+  'geometry/Point',
+  'geometry/Size',
+  'geometry/Rect',
+
+  'widgets/ILayout',
+  'widgets/ILayoutItem',
+  'widgets/Alignment',
+  'widgets/Orientation',
+  'widgets/SizePolicy',
+  'widgets/WidgetFlag',
+  'widgets/ChildEvent',
+  'widgets/MoveEvent',
+  'widgets/ResizeEvent',
+  'widgets/SizingItem',
+  'widgets/layoutCalc',
+  'widgets/WidgetItem',
+  'widgets/Layout',
+  'widgets/Widget',
+  'widgets/SplitterHandle',
+  'widgets/SplitterLayout',
+  'widgets/Splitter'
+].map(function(name) { return './src/' + name + '.ts'; });
 
 
-var project = typescript.createProject({
-  declarationFiles: true,
-  noImplicitAny: true,
-  target: 'ES5',
-});
+var stylSources = './styl/index.styl';
 
 
 gulp.task('clean', function(cb) {
-  del([buildDir, distDir], cb);
+  del(['./dist'], cb);
 });
 
 
-gulp.task('build', function() {
-  var src = gulp.src(tsSources).pipe(typescript(project));
-  var dts = src.dts.pipe(concat('phosphor.d.ts')).pipe(gulp.dest(dtsBuildDir));
-  var js = src.pipe(concat('phosphor.js')).pipe(gulp.dest(jsBuildDir));
+gulp.task('dist', function() {
+  var project = typescript.createProject({
+    declarationFiles: true,
+    noImplicitAny: true,
+    target: 'ES5',
+  });
+
+  var src = gulp.src(tsSources)
+    .pipe(typescript(project));
+
+  var dts = src.dts.pipe(concat('phosphor.d.ts'))
+    .pipe(gulp.dest('./dist'));
+
+  var js = src.pipe(concat('phosphor.js'))
+    .pipe(gulp.dest('./dist'));
+
   var css = gulp.src(stylSources)
     .pipe(stylus({ use: [nib()] }))
     .pipe(rename('phosphor.css'))
-    .pipe(gulp.dest(cssBuildDir));
+    .pipe(gulp.dest('./dist'));
+
   return stream.merge(dts, js, css);
 });
 
 
-gulp.task('dist', ['build'], function() {
-  // TODO implement me
-});
-
-
 gulp.task('watch', function() {
-  gulp.watch(tsFiles, ['build']);
+  gulp.watch(tsSources, ['dist']);
 });
 
 
@@ -70,20 +114,19 @@ gulp.task('examples', function() {
     noImplicitAny: true,
     target: 'ES5',
   });
-  var glob = [].concat([
-    'build/dts/phosphor.d.ts',
-    'examples/**/index.ts'
-  ]);
-  var src = gulp.src(glob)
+
+  var src = gulp.src(['dist/phosphor.d.ts', 'examples/**/index.ts'])
     .pipe(typescript(project))
     .pipe(rename(function (path) {
-      path.dirname += '/build';
-    })).pipe(gulp.dest('examples'));
+      path.dirname += '/build'; }))
+    .pipe(gulp.dest('examples'));
+
   var css = gulp.src('examples/**/index.styl')
     .pipe(stylus({use: [nib()]}))
     .pipe(rename(function (path) {
-      path.dirname += '/build';
-    })).pipe(gulp.dest('examples'));
+      path.dirname += '/build'; }))
+    .pipe(gulp.dest('examples'));
+
   return stream.merge(src, css);
 });
 
