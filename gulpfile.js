c2b83f4a4a530f164e99bf2ae33d655c6f7ac2f9
S. Chris Colbert <sccolbert@gmail.com>
2015-03-17 00:10:39 -0400

    temporarily disable shell build

--- gulpfile.js
+++ gulpfile.js
@@ -108,14 +108,14 @@ var tsSources = [
   'widgets/TabWidget',
   'widgets/DockArea',
 
-  'shell/IPlugin',
-  'shell/IPluginList',
-  'shell/IRegion',
-  'shell/IRegionManager',
-  'shell/AutoHidePanel',
-  'shell/PluginList',
-  'shell/RegionManager',
-  'shell/Bootstrapper'
+  // 'shell/IPlugin',
+  // 'shell/IPluginList',
+  // 'shell/IRegion',
+  // 'shell/IRegionManager',
+  // 'shell/AutoHidePanel',
+  // 'shell/PluginList',
+  // 'shell/RegionManager',
+  // 'shell/Bootstrapper'
 ].map(function(name) { return './src/' + name + '.ts'; });
 
 
