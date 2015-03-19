f0a5a25f0f43ce51be86e951c9e97f89bf162d13
Chris Colbert <sccolbert@gmail.com>
2015-03-19 14:56:52 -0400

    fix #4 - rename slot -> callback

--- src/core/Signal.ts
+++ src/core/Signal.ts
@@ -11,10 +11,8 @@ module phosphor.core {
  * An object used for loosely coupled inter-object communication.
  *
  * A signal is emitted by an object in response to some event. User
- * code may connect listener functions (slots) to the signal to be
- * notified when that event occurs. This is a simple and efficient
- * form of the pub-sub pattern which promotes type-safe and loosely
- * coupled communication between objects.
+ * code may connect callback functions to the signal to be notified
+ * when that event occurs.
  */
 export
 class Signal<T, U> {
@@ -24,88 +22,87 @@ class Signal<T, U> {
   constructor() { }
 
   /**
-   * Connect a slot to the signal.
+   * Connect a callback to the signal.
    *
-   * Slot connections are not de-duplicated. If the slot is connected
-   * to the signal multiple times, it will be invoked multiple times
-   * when the signal is emitted.
+   * If the callback is connected to the signal multiple times, it
+   * will be invoked that many times when the signal is emitted.
    *
-   * It is safe to connect a slot while the signal is being emitted.
-   * The slot will be invoked the next time the signal is emitted.
+   * It is safe to connect the callback to the signal while the signal
+   * is being emitted. The callback will not be invoked until the next
+   * time the signal is emitted.
    */
-  connect(slot: (sender: T, args: U) => void, thisArg?: any): void {
-    var wrapper = new SlotWrapper(slot, thisArg);
-    var slots = this._m_slots;
-    if (slots === null) {
-      this._m_slots = wrapper;
-    } else if (slots instanceof SlotWrapper) {
-      this._m_slots = [slots, wrapper];
+  connect(cb: (sender: T, args: U) => void, thisArg?: any): void {
+    var wrapper = new CBWrapper(cb, thisArg);
+    var current = this._m_callbacks;
+    if (current === null) {
+      this._m_callbacks = wrapper;
+    } else if (current instanceof CBWrapper) {
+      this._m_callbacks = [current, wrapper];
     } else {
-      (<SlotWrapper<T, U>[]>slots).push(wrapper);
+      (<CBWrapper<T, U>[]>current).push(wrapper);
     }
   }
 
   /**
-   * Disconnect a slot from the signal.
+   * Disconnect a callback from the signal.
    *
-   * This will remove all connections to the slot, even if the slot
-   * was connected multiple times. If no slot is provided, all slots
-   * will be disconnected.
+   * This will remove all instances of the callback from the signal.
+   * If no callback is provided, all callbacks will be disconnected.
    *
-   * It is safe to disconnect a slot while the signal is being emitted.
-   * The slot is removed immediately and will not be invoked.
+   * It is safe to disconnect a callback from the signal while the
+   * signal is being emitted. The callback will not be invoked.
    */
-  disconnect(slot?: (sender: T, args: U) => void, thisArg?: any): void {
-    var slots = this._m_slots;
-    if (slots === null) {
+  disconnect(cb?: (sender: T, args: U) => void, thisArg?: any): void {
+    var current = this._m_callbacks;
+    if (current === null) {
       return;
     }
-    if (slots instanceof SlotWrapper) {
-      if (!slot || slots.equals(slot, thisArg)) {
-        slots.clear();
-        this._m_slots = null;
+    if (current instanceof CBWrapper) {
+      if (!cb || current.equals(cb, thisArg)) {
+        current.clear();
+        this._m_callbacks = null;
       }
-    } else if (!slot) {
-      var array = <SlotWrapper<T, U>[]>slots;
+    } else if (!cb) {
+      var array = <CBWrapper<T, U>[]>current;
       for (var i = 0, n = array.length; i < n; ++i) {
         array[i].clear();
       }
-      this._m_slots = null;
+      this._m_callbacks = null;
     } else {
-      var rest: SlotWrapper<T, U>[] = [];
-      var array = <SlotWrapper<T, U>[]>slots;
+      var rest: CBWrapper<T, U>[] = [];
+      var array = <CBWrapper<T, U>[]>current;
       for (var i = 0, n = array.length; i < n; ++i) {
         var wrapper = array[i];
-        if (wrapper.equals(slot, thisArg)) {
+        if (wrapper.equals(cb, thisArg)) {
           wrapper.clear();
         } else {
           rest.push(wrapper);
         }
       }
       if (rest.length === 0) {
-        this._m_slots = null;
+        this._m_callbacks = null;
       } else if (rest.length === 1) {
-        this._m_slots = rest[0];
+        this._m_callbacks = rest[0];
       } else {
-        this._m_slots = rest;
+        this._m_callbacks = rest;
       }
     }
   }
 
   /**
-   * Test whether a slot is connected to the signal.
+   * Test whether a callback is connected to the signal.
    */
-  isConnected(slot: (sender: T, args: U) => void, thisArg?: any): boolean {
-    var slots = this._m_slots;
-    if (slots === null) {
+  isConnected(cb: (sender: T, args: U) => void, thisArg?: any): boolean {
+    var current = this._m_callbacks;
+    if (current === null) {
       return false;
     }
-    if (slots instanceof SlotWrapper) {
-      return slots.equals(slot, thisArg);
+    if (current instanceof CBWrapper) {
+      return current.equals(cb, thisArg);
     }
-    var array = <SlotWrapper<T, U>[]>slots;
+    var array = <CBWrapper<T, U>[]>current;
     for (var i = 0, n = array.length; i < n; ++i) {
-      if (array[i].equals(slot, thisArg)) {
+      if (array[i].equals(cb, thisArg)) {
         return true;
       }
     }
@@ -113,68 +110,68 @@ class Signal<T, U> {
   }
 
   /**
-   * Emit the signal and invoke its connected slots.
+   * Emit the signal and invoke its connected callbacks.
    *
-   * Slots are invoked in the order in which they are connected.
+   * Callbacks are invoked in the order in which they are connected.
    */
   emit(sender: T, args: U): void {
-    var slots = this._m_slots;
-    if (slots === null) {
+    var current = this._m_callbacks;
+    if (current === null) {
       return;
     }
-    if (slots instanceof SlotWrapper) {
-      slots.invoke(sender, args);
+    if (current instanceof CBWrapper) {
+      current.invoke(sender, args);
     } else {
-      var array = <SlotWrapper<T, U>[]>slots;
+      var array = <CBWrapper<T, U>[]>current;
       for (var i = 0, n = array.length; i < n; ++i) {
         array[i].invoke(sender, args);
       }
     }
   }
 
-  private _m_slots: SlotWrapper<T, U> | SlotWrapper<T, U>[] = null;
+  private _m_callbacks: CBWrapper<T, U> | CBWrapper<T, U>[] = null;
 }
 
 
 /**
- * A thin wrapper around a slot function and context object.
+ * A thin wrapper around a callback function and context.
  */
-class SlotWrapper<T, U> {
+class CBWrapper<T, U> {
   /**
-   * Construct a new slot wrapper.
+   * Construct a new callback wrapper.
    */
-  constructor(slot: (sender: T, args: U) => void, thisArg: any) {
-    this._m_slot = slot;
+  constructor(cb: (sender: T, args: U) => void, thisArg: any) {
+    this._m_cb = cb;
     this._m_thisArg = thisArg;
   }
 
   /**
-   * Clear the contents of the slot wrapper.
+   * Clear the contents of the callback wrapper.
    */
   clear(): void {
-    this._m_slot = null;
+    this._m_cb = null;
     this._m_thisArg = null;
   }
 
   /**
-   * Test whether the wrapper equals a slot and context.
+   * Test whether the wrapper equals a callback and context.
    */
-  equals(slot: (sender: T, args: U) => void, thisArg: any): boolean {
-    return this._m_slot === slot && this._m_thisArg === thisArg;
+  equals(cb: (sender: T, args: U) => void, thisArg: any): boolean {
+    return this._m_cb === cb && this._m_thisArg === thisArg;
   }
 
   /**
-   * Invoke the wrapper slot with the given sender and args.
+   * Invoke the wrapped callback with the given sender and args.
    *
    * This is a no-op if the wrapper has been cleared.
    */
   invoke(sender: T, args: U): void {
-    if (this._m_slot) {
-      this._m_slot.call(this._m_thisArg, sender, args);
+    if (this._m_cb) {
+      this._m_cb.call(this._m_thisArg, sender, args);
     }
   }
 
-  private _m_slot: (sender: T, args: U) => void;
+  private _m_cb: (sender: T, args: U) => void;
   private _m_thisArg: any;
 }
 
