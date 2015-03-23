46b7a32960ae111cd74bdee8aa70f7b4b6484bbe
Chris Colbert <sccolbert@gmail.com>
2015-03-23 15:22:34 -0400

    update split panel example

--- examples/splitpanel/index.ts
+++ examples/splitpanel/index.ts
@@ -7,43 +7,41 @@
 |----------------------------------------------------------------------------*/
 module example {
 
-import Orientation = phosphor.enums.Orientation;
+import Orientation = phosphor.panels.Orientation;
+import Panel = phosphor.panels.Panel;
+import SplitPanel = phosphor.panels.SplitPanel;
 
-import Splitter = phosphor.widgets.Splitter;
-import Widget = phosphor.widgets.Widget;
 
-
-function createContentWidget(name: string): Widget {
-  var w = new Widget();
-  w.classList.add(name);
-  w.minWidth = 50;
-  w.minHeight = 50;
-  return w;
+function createContent(name: string): Panel {
+  var panel = new Panel();
+  panel.node.classList.add(name);
+  panel.setMinSize(50, 50);
+  return panel;
 }
 
 
 function main(): void {
-  var sp1 = new Splitter(Orientation.Vertical);
-  var sp2 = new Splitter(Orientation.Horizontal);
-  var sp3 = new Splitter(Orientation.Vertical);
+  var sp1 = new SplitPanel(Orientation.Vertical);
+  var sp2 = new SplitPanel(Orientation.Horizontal);
+  var sp3 = new SplitPanel(Orientation.Vertical);
 
-  sp3.addWidget(createContentWidget('red'));
-  sp3.addWidget(createContentWidget('green'));
-  sp3.addWidget(createContentWidget('blue'));
+  sp3.add(createContent('red'));
+  sp3.add(createContent('green'));
+  sp3.add(createContent('blue'));
 
-  sp2.addWidget(sp3);
-  sp2.addWidget(createContentWidget('yellow'));
-  sp2.addWidget(createContentWidget('red'));
+  sp2.add(sp3);
+  sp2.add(createContent('yellow'));
+  sp2.add(createContent('red'));
 
-  sp1.addWidget(createContentWidget('yellow'));
-  sp1.addWidget(createContentWidget('blue'));
-  sp1.addWidget(sp2);
-  sp1.addWidget(createContentWidget('green'));
+  sp1.add(createContent('yellow'));
+  sp1.add(createContent('blue'));
+  sp1.add(sp2);
+  sp1.add(createContent('green'));
 
   sp1.attach(document.getElementById('main'));
-  sp1.fitToHost();
+  sp1.fit();
 
-  window.onresize = () => sp1.fitToHost();
+  window.onresize = () => sp1.fit();
 }
 
 window.onload = main;
