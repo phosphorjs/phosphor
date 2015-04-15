c32b8267f0de989b2425c6d63641cfb1166cb857
Chris Colbert <sccolbert@gmail.com>
2015-04-15 13:01:39 -0400

    fill in shell panel methods

--- src/shell/shellpanel.ts
+++ src/shell/shellpanel.ts
@@ -7,9 +7,16 @@
 |----------------------------------------------------------------------------*/
 module phosphor.shell {
 
+import algo = collections.algorithm;
+
+import Pair = utility.Pair;
+
 import Alignment = widgets.Alignment;
+import BoxLayout = widgets.BoxLayout;
+import ChildMessage = widgets.ChildMessage;
 import Direction = widgets.Direction;
 import Widget = widgets.Widget;
+import WidgetFlag = widgets.WidgetFlag;
 
 
 /**
@@ -24,37 +31,76 @@ var SHELL_PANEL_CLASS = 'p-ShellPanel';
 export
 interface IWidgetOptions {
   /**
-   * The rank for the widget.
-   *
-   * Widgets are arranged in the panel from lowest to highest rank.
+   * The layout rank for the widget.
    */
   rank?: number;
 
   /**
-   * The stretch factor for the widget in the panel.
+   * The layout stretch factor for the widget.
    */
   stretch?: number;
 
   /**
-   * The alignment for the widget in the panel.
+   * The layout alignment for the widget.
    */
   alignment?: Alignment;
 }
 
 
 /**
+ * A content panel for a shell view.
  *
+ * A shell panel used to host widgets which are ordered by rank. It
+ * is typically used as a content area for plugin-provided widgets.
  */
 export
 class ShellPanel extends Widget {
   /**
-   *
+   * Construct a new shell view.
    */
   constructor(direction: Direction) {
     super();
     this.addClass(SHELL_PANEL_CLASS);
+    this.layout = new BoxLayout(direction, 0);
+    this.setFlag(WidgetFlag.DisallowLayoutChange);
   }
 
+  /**
+   * Dispose of the resources held by the widget.
+   */
+  dispose(): void {
+    this._pairs = null;
+    super.dispose();
+  }
+
+  /**
+   * Add a widget to the panel.
+   *
+   * Widgets are ordered from lowest to highest rank in the direction
+   * of the panel layout. The default rank for a widget is `100`.
+   */
+  addWidget(widget: Widget, options: IWidgetOptions = {}): void {
+    widget.parent = null;
+    var stretch = options.stretch;
+    var alignment = options.alignment;
+    var rank = options.rank !== void 0 ? options.rank : 100;
+    var i = algo.findIndex(this._pairs, pair => pair.second > rank);
+    if (i === -1) i = this._pairs.length;
+    algo.insert(this._pairs, i, new Pair(widget, rank));
+    (<BoxLayout>this.layout).insertWidget(i, widget, stretch, alignment);
+  }
+
+  /**
+   * A method invoked when a 'child-removed' message is received.
+   */
+  protected onChildRemoved(msg: ChildMessage): void {
+    super.onChildRemoved(msg);
+    var widget = msg.child;
+    var i = algo.findIndex(this._pairs, pair => pair.first === widget);
+    if (i !== -1) algo.removeAt(this._pairs, i);
+  }
+
+  _pairs: Pair<Widget, number>[] = [];
 }
 
 } // module phosphor.shell
