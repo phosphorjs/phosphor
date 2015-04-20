bf84f39a0a9333ed3be12e3592669bb4a0f943a6
S. Chris Colbert <sccolbert@gmail.com>
2015-04-20 19:07:46 -0400

    update chat app example to use RenderWidget

--- examples/chatapp/chat/feedplugin.ts
+++ examples/chatapp/chat/feedplugin.ts
@@ -11,13 +11,15 @@ import IContainer = phosphor.di.IContainer;
 
 import IShellView = phosphor.shell.IShellView;
 
+import Size = phosphor.utility.Size;
+
 import Component = phosphor.virtualdom.Component;
 import Elem = phosphor.virtualdom.Elem;
 import IData = phosphor.virtualdom.IData;
 import createFactory = phosphor.virtualdom.createFactory;
 import dom = phosphor.virtualdom.dom;
 
-import ElementHost = phosphor.widgets.ElementHost;
+import RenderWidget = phosphor.widgets.RenderWidget;
 
 
 /**
@@ -43,16 +45,32 @@ var Feed = createFactory(FeedPlaceholder);
 
 
 /**
+ * A host widget for the feed component.
+ */
+class FeedHost extends RenderWidget {
+
+  constructor() {
+    super();
+    this.node.classList.add('chat-feed-host');
+  }
+
+  sizeHint(): Size {
+    return new Size(600, 200);
+  }
+
+  protected render(): Elem {
+    return Feed();
+  }
+}
+
+
+/**
  * Initialize the chat feed plugin.
  */
 export
 function initialize(container: IContainer): void {
   var shell = container.resolve(IShellView);
-
-  var feed = new ElementHost(Feed(), 600, 200);
-  feed.addClass('chat-feed-host');
-
-  shell.addWidget('bottom', feed);
+  shell.addWidget('bottom', new FeedHost());
 }
 
 } // module chat.feedplugin
