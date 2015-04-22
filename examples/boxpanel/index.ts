c16ad5294812f0628102d9e1fd4167e3ca466d7b
Chris Colbert <sccolbert@gmail.com>
2015-04-22 15:04:21 -0400

    make use of NodeBase methods where possible

--- examples/boxpanel/index.ts
+++ examples/boxpanel/index.ts
@@ -16,8 +16,8 @@ import Widget = phosphor.widgets.Widget;
 
 function createContent(name: string): Widget {
   var widget = new Widget();
-  widget.node.classList.add('content');
-  widget.node.classList.add(name);
+  widget.addClass('content');
+  widget.addClass(name);
   return widget;
 }
 
