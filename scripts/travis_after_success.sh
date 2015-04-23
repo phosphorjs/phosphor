7cc70fd673551dd05d3074f9996ce18ad49b8844
Chris Colbert <sccolbert@gmail.com>
2015-04-23 12:31:02 -0400

    fix (hopefully) docs build creds

--- scripts/travis_after_success.sh
+++ scripts/travis_after_success.sh
@@ -3,11 +3,8 @@ if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == "master" ]]
 then
     echo "-- will build docs --"
 
-    echo "https://${GHTOKEN}:@github.com" > .git/credentials
-
     git config --global user.email "travis@travis-ci.com"
     git config --global user.name "Travis Bot"
-    git config credential.helper "store --file=.git/credentials"
 
     rm -rf build/docs/
     gulp docs
@@ -15,6 +12,10 @@ then
     git clone https://github.com/phosphorjs/phosphor.git travis_docs_build
     cd travis_docs_build
     git checkout gh-pages
+
+    echo "https://${GHTOKEN}:@github.com" > .git/credentials
+    git config credential.helper "store --file=.git/credentials"
+
     rm -rf ./*
     cp -r ../build/docs/* ./.
     git add -A
