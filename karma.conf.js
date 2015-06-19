1338c420c834119f4d6191fd62650c41d1708c8e
Steven Silvester <steven.silvester@ieee.org>
2015-06-19 13:29:51 -0500

    Try to fix karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -1,64 +1,28 @@
 module.exports = function (config) {
-  'use strict';
-  config.set({
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+    'use strict';
+    config.set({
 
-    frameworks: ['mocha'],
+        basePath: '',
 
-    // list of files / patterns to load in the browser
-    files: [
-      "node_modules/expect.js/index.js",
-      "dist/phosphor.js",
-      "tests/build/index.js"
-    ],
+        frameworks: ['mocha'],
 
-    // list of files to exclude
-    exclude: [
-    ],
+        files: [
+            'dist/*.js',
+            'tests/build/*.js'
+        ],
 
-    preprocessors: {
-    },
+        reporters: ['progress'],
 
-    // use dots reporter, as travis terminal does not support escaping sequences
-    // possible values: 'dots', 'progress'
-    // CLI --reporters progress
-    reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        autoWatch: false,
+        singleRun: false,
 
-    // web server port
-    // CLI --port 9876
-    port: 9876,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // enable / disable colors in the output (reporters and logs)
-    // CLI --colors --no-colors
-    colors: true,
+        browsers: ['Firefox']
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    // CLI --log-level debug
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    // CLI --auto-watch --no-auto-watch
-    autoWatch: true,
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    // CLI --browsers Chrome,Firefox,Safari
-    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
-
-    // If browser does not capture in given timeout [ms], kill it
-    // CLI --capture-timeout 5000
-    captureTimeout: 20000,
-
-    // report which specs are slower than 500ms
-    // CLI --report-slower-than 500
-    reportSlowerThan: 500,
-  })
-}
+    });
+};
