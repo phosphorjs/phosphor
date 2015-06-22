e339d7eae4e2cc766c133f3864212cd81602519b
Steven Silvester <steven.silvester@ieee.org>
2015-06-22 15:35:28 -0500

    Enforce the typings

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # make sure we have the typings
-ls typings/codemirror  
+ls typings/codemirror || exit 1
 gulp
 gulp tests
 ./node_modules/.bin/karma start --browsers=Firefox tests/karma.conf.js
