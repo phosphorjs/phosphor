cf3e24bb56afed22bf75a523d0bf29db7b886289
Steven Silvester <steven.silvester@ieee.org>
2015-06-23 07:25:27 -0500

    Add all of the typing files

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="codemirror/codemirror.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="expect.js/expect.js.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
