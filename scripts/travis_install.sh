a3374dbfa26ff682d69bff6a6280f7d44ca69b93
S. Chris Colbert <sccolbert@gmail.com>
2015-07-15 02:21:52 -0400

    remove use of tsd in build

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 npm install -g gulp
 npm install -g typedoc
-npm install -g tsd@next
 npm install
-
-echo "{\"token\": \"$GHTOKEN\"}" > ~/.tsdrc
-
-# Pull typings from the cache and attempt to get latest
-cp -r $HOME/typings .
-tsd reinstall -so; true
-# Put any new typings in the cache
-cp -r typings $HOME
