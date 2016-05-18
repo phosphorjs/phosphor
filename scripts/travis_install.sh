bede66c3881077c9723c51050ef8fdff12ba94f1
S. Chris Colbert <sccolbert@gmail.com>
2016-05-18 08:31:10 -0500

    remove old code

--- scripts/travis_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npm install -g gulp
-npm install -g typedoc
-npm install
--- scripts/travis_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-gulp
-gulp examples
-gulp tests
-./node_modules/.bin/karma start --browsers=Firefox tests/karma.conf.js
