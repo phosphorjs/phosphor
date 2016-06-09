bbbe1b5ba1417d8c0ddffeb7bc978099497e6e7c
A. Darian <git@darian.af>
2016-06-09 20:38:29 +0100

    CommandItem tests

--- test/src/ui/commandpalette.spec.ts
+++ test/src/ui/commandpalette.spec.ts
@@ -19,6 +19,10 @@ import {
   commands, ICommand
 } from '../../../lib/ui/commands';
 
+import {
+  keymap, KeyBinding
+} from '../../../lib/ui/keymap';
+
 
 describe('ui/commandpalette', () => {
 
@@ -45,7 +49,7 @@ describe('ui/commandpalette', () => {
       it('should be read-only', () => {
         let options: CommandItem.IOptions = { command: 'test' };
         let item = new CommandItem(options);
-        expect(() => { item.command = null }).to.throwError();
+        expect(() => { item.command = 'test-1' }).to.throwError();
       });
 
     });
@@ -78,15 +82,188 @@ describe('ui/commandpalette', () => {
         let options: ICommand = { execute: () => { }, label: 'test label' };
         let command = commands.addCommand('test', options);
         let item = new CommandItem({ command: 'test' });
-        expect(item.label).to.eql(options.label);
+        expect(item.label).to.be(options.label);
         command.dispose();
       });
 
       it('should be read-only', () => {
-        let options: ICommand = { execute: () => { }, label: 'test label' };
+        let options: ICommand = { execute: () => { } };
         let command = commands.addCommand('test', options);
         let item = new CommandItem({ command: 'test' });
-        expect(() => { item.label = null }).to.throwError();
+        expect(() => { item.label = 'test label' }).to.throwError();
+        command.dispose();
+      });
+
+    });
+
+    describe('#caption', () => {
+
+      it('should return the caption of a command item', () => {
+        let options: ICommand = { execute: () => { }, caption: 'test caption' };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(item.caption).to.be(options.caption);
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.caption = 'test caption' }).to.throwError();
+        command.dispose();
+      });
+
+    });
+
+    describe('#className', () => {
+
+      it('should return the class name of a command item', () => {
+        let options: ICommand = { execute: () => { }, className: 'testClass' };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(item.className).to.be(options.className);
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.className = 'testClass' }).to.throwError();
+        command.dispose();
+      });
+
+    });
+
+    describe('#isEnabled', () => {
+
+      it('should return whether a command item is enabled', () => {
+        let called = false;
+        let options: ICommand = {
+          execute: () => { },
+          isEnabled: () => called = !called
+        };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(called).to.be(false);
+        expect(item.isEnabled).to.be(true);
+        expect(called).to.be(true);
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.isEnabled = false }).to.throwError();
+        command.dispose();
+      });
+
+    });
+
+    describe('#isToggled', () => {
+
+      it('should return whether a command item is toggled', () => {
+        let called = false;
+        let options: ICommand = {
+          execute: () => { },
+          isToggled: () => called = !called
+        };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(called).to.be(false);
+        expect(item.isToggled).to.be(true);
+        expect(called).to.be(true);
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.isToggled = false }).to.throwError();
+        command.dispose();
+      });
+
+    });
+
+    describe('#isVisible', () => {
+
+      it('should return whether a command item is visible', () => {
+        let called = false;
+        let options: ICommand = {
+          execute: () => { },
+          isVisible: () => called = !called
+        };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(called).to.be(false);
+        expect(item.isVisible).to.be(true);
+        expect(called).to.be(true);
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.isVisible = false }).to.throwError();
+        command.dispose();
+      });
+
+    });
+
+    describe('#keyBinding', () => {
+
+      it('should return the key binding of a command item', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let binding = keymap.addBinding({
+          keys: ['Ctrl A'],
+          selector: 'body',
+          command: 'test'
+        });
+        let item = new CommandItem({ command: 'test' });
+        expect(item.keyBinding).to.be.a(KeyBinding);
+        expect(item.keyBinding.keys).to.eql(['Ctrl A']);
+        binding.dispose();
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.keyBinding = null }).to.throwError();
+        command.dispose();
+      });
+
+    });
+
+    describe('#category', () => {
+
+      it('should return the category of a command item', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test', category: 'random' });
+        expect(item.category).to.be('random');
+        command.dispose();
+      });
+
+      it('should default the category to `"general"`', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(item.category).to.be('general');
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { } };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.category = 'random' }).to.throwError();
         command.dispose();
       });
 
