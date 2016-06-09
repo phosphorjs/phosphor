f0d89e6ec8a0155f2fbc8b441c06860d32e1c382
Steven Silvester <steven.silvester@ieee.org>
2016-06-09 13:38:18 -0500

    wip stacked panel tests

---
+++ test/src/ui/stackedpanel.spec.ts
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2016, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+import expect = require('expect.js');
+
+import {
+  StackedLayout, StackedPanel
+} from '../../../lib/ui/stackedpanel';
+
+
+describe('ui/stackedpanel', () => {
+
+  describe('StackedPanel', () => {
+
+  });
+
+});
