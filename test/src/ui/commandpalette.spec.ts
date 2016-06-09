390d60072aeb10f71e283491aeed40f0bea73248
A. Darian <git@darian.af>
2016-06-09 18:43:37 +0100

    wip

--- test/src/ui/commandpalette.spec.ts
+++ test/src/ui/commandpalette.spec.ts
@@ -8,13 +8,30 @@
 import expect = require('expect.js');
 
 import {
+  commands
+} from '../../../lib/ui/commands';
+
+import {
   CommandItem, CommandPalette
 } from '../../../lib/ui/commandpalette';
 
 
 describe('ui/commandpalette', () => {
 
-  describe('CommandItem', () => { });
+  describe('CommandItem', () => {
+
+    describe('#constructor()', () => {
+
+      it('should accept a command item options argument', () => {
+        let command = commands.addCommand('test', { execute: () => { } });
+        let options = { command: 'test', category: 'test' };
+        let item = new CommandItem(options as CommandItem.IOptions);
+        command.dispose();
+      });
+
+    });
+
+  });
 
   describe('CommandPalette', () => { });
 
