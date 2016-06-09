516e4305b49eefdc4315e2063e80d90ac7042de1
A. Darian <git@darian.af>
2016-06-09 19:11:26 +0100

    work in progress

--- test/src/ui/commandpalette.spec.ts
+++ test/src/ui/commandpalette.spec.ts
@@ -8,12 +8,16 @@
 import expect = require('expect.js');
 
 import {
-  commands
-} from '../../../lib/ui/commands';
+  JSONObject
+} from '../../../lib/algorithm/json';
 
 import {
   CommandItem, CommandPalette
 } from '../../../lib/ui/commandpalette';
+
+import {
+  commands, ICommand
+} from '../../../lib/ui/commands';
 
 
 describe('ui/commandpalette', () => {
@@ -23,9 +27,66 @@ describe('ui/commandpalette', () => {
     describe('#constructor()', () => {
 
       it('should accept a command item options argument', () => {
-        let command = commands.addCommand('test', { execute: () => { } });
-        let options = { command: 'test', category: 'test' };
-        let item = new CommandItem(options as CommandItem.IOptions);
+        let options: CommandItem.IOptions = { command: 'test' };
+        let item = new CommandItem(options);
+        expect(item).to.be.a(CommandItem);
+      });
+
+    });
+
+    describe('#command', () => {
+
+      it('should return the command name of a command item', () => {
+        let options: CommandItem.IOptions = { command: 'test' };
+        let item = new CommandItem(options);
+        expect(item.command).to.be('test');
+      });
+
+      it('should be read-only', () => {
+        let options: CommandItem.IOptions = { command: 'test' };
+        let item = new CommandItem(options);
+        expect(() => { item.command = null }).to.throwError();
+      });
+
+    });
+
+    describe('#args', () => {
+
+      it('should return the args of a command item', () => {
+        let options: CommandItem.IOptions = {
+          args: { foo: 'bar', baz: 'qux' } as JSONObject,
+          command: 'test'
+        };
+        let item = new CommandItem(options);
+        expect(item.args).to.eql(options.args);
+      });
+
+      it('should be read-only', () => {
+        let options: CommandItem.IOptions = {
+          args: { foo: 'bar', baz: 'qux' } as JSONObject,
+          command: 'test'
+        };
+        let item = new CommandItem(options);
+        expect(() => { item.args = null }).to.throwError();
+      });
+
+    });
+
+    describe('#label', () => {
+
+      it('should return the label of a command item', () => {
+        let options: ICommand = { execute: () => { }, label: 'test label' };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(item.label).to.eql(options.label);
+        command.dispose();
+      });
+
+      it('should be read-only', () => {
+        let options: ICommand = { execute: () => { }, label: 'test label' };
+        let command = commands.addCommand('test', options);
+        let item = new CommandItem({ command: 'test' });
+        expect(() => { item.label = null }).to.throwError();
         command.dispose();
       });
 
