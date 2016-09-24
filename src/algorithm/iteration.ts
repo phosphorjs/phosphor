fc931902e032eb26f05b5346f01b54109923f7f9
S. Chris Colbert <sccolbert@gmail.com>
2016-09-23 23:26:51 -0500

    add a repeat iterator

--- src/algorithm/iteration.ts
+++ src/algorithm/iteration.ts
@@ -620,6 +620,75 @@ class EnumerateIterator<T> implements IIterator<[number, T]> {
 
 
 /**
+ * Create an iterator which repeats a value a number of times.
+ *
+ * @param value - The value to repeat.
+ *
+ * @param count - The number of times to repeat the value.
+ *
+ * @returns A new iterator which repeats the specified value.
+ */
+export
+function repeat<T>(value: T, count: number): RepeatIterator<T> {
+  return new RepeatIterator<T>(value, count);
+}
+
+
+/**
+ * An iterator which repeats a value a specified number of times.
+ */
+export
+class RepeatIterator<T> implements IIterator<T> {
+  /**
+   * Construct a new repeat iterator.
+   *
+   * @param value - The value to repeat.
+   *
+   * @param count - The number of times to repeat the value.
+   */
+  constructor(value: T, count: number) {
+    this._value = value;
+    this._count = count;
+  }
+
+  /**
+   * Create an iterator over the object's values.
+   *
+   * @returns A reference to `this` iterator.
+   */
+  iter(): this {
+    return this;
+  }
+
+  /**
+   * Create an independent clone of the repeat iterator.
+   *
+   * @returns A new iterator starting with the current value.
+   */
+  clone(): RepeatIterator<T> {
+    return new RepeatIterator(this._value, this._count);
+  }
+
+  /**
+   * Get the next value from the iterator.
+   *
+   * @returns The next value from the iterator, or `undefined` if
+   *   the iterator is exhausted.
+   */
+  next(): T {
+    if (this._count <= 0) {
+      return void 0;
+    }
+    this._count--;
+    return this._value;
+  }
+
+  private _value: T;
+  private _count: number;
+}
+
+
+/**
  * Chain together several iterables.
  *
  * @param objects - The iterables or array-like objects of interest.
