43b5652137887efd2b7bf1c542a998fe5cfd226b
S. Chris Colbert <sccolbert@gmail.com>
2016-12-08 16:28:03 -0600

    add index file

---
+++ src/collections/src/index.ts
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2016, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+export * from './deque';
+export * from './queue';
+export * from './stack';
+export * from './vector';
