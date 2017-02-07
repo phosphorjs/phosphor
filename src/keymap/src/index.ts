af197d6f88e1ca0b9e6d80581d7286553cba050d
S. Chris Colbert <sccolbert@gmail.com>
2017-02-07 00:45:42 -0600

    merge keymap and command registry

--- src/keymap/src/index.ts
+++
@@ -1,9 +0,0 @@
-/*-----------------------------------------------------------------------------
-| Copyright (c) 2014-2016, PhosphorJS Contributors
-|
-| Distributed under the terms of the BSD 3-Clause License.
-|
-| The full license is in the file LICENSE, distributed with this software.
-|----------------------------------------------------------------------------*/
-export * from './keyboard';
-export * from './keymap';
