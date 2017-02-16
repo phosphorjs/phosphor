be3480088ce9b8376e6ead1e801380b8b3f55c98
S. Chris Colbert <sccolbert@gmail.com>
2017-02-15 22:19:24 -0600

    silence test/docs build

--- scripts/travis_after_success.sh
+++ scripts/travis_after_success.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # submit coverage to coveralls
-cat ./test/coverage/Firefox*/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+# cat ./test/coverage/Firefox*/lcov.info | ./node_modules/coveralls/bin/coveralls.js
 
 # push docs on master branch
 if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == "master" ]]
 then
     echo "-- will push docs --"
 
-    git config --global user.email "travis@travis-ci.com"
-    git config --global user.name "Travis Bot"
+    # git config --global user.email "travis@travis-ci.com"
+    # git config --global user.name "Travis Bot"
 
-    git clone https://${GHREPO}.git travis_docs_build
-    cd travis_docs_build
-    git checkout gh-pages
+    # git clone https://${GHREPO}.git travis_docs_build
+    # cd travis_docs_build
+    # git checkout gh-pages
 
-    echo "https://${GHTOKEN}:@github.com" > .git/credentials
-    git config credential.helper "store --file=.git/credentials"
+    # echo "https://${GHTOKEN}:@github.com" > .git/credentials
+    # git config credential.helper "store --file=.git/credentials"
 
-    rm -rf ./*
-    cp -r ../docs/* ./.
-    touch .nojekyll
-    git add -A
-    git commit -m "autocommit docs"
-    git push origin gh-pages
+    # rm -rf ./*
+    # cp -r ../docs/* ./.
+    # touch .nojekyll
+    # git add -A
+    # git commit -m "autocommit docs"
+    # git push origin gh-pages
 else
     echo "-- will only push docs from master --"
 fi
