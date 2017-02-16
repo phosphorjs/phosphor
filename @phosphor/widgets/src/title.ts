a9829c6576fc49e0c2d9e4279087f6f05fb2fa1f
S. Chris Colbert <sccolbert@gmail.com>
2017-02-15 23:16:13 -0600

    update copyright headers

--- @phosphor/widgets/src/title.ts
+++ @phosphor/widgets/src/title.ts
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------
-| Copyright (c) 2014-2016, PhosphorJS Contributors
+| Copyright (c) 2014-2017, PhosphorJS Contributors
 |
 | Distributed under the terms of the BSD 3-Clause License.
 |
