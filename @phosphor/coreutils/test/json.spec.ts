100dd9c2e9b3c75d03c8238e65db1701bc26d628
S. Chris Colbert <sccolbert@gmail.com>
2017-02-27 13:01:02 -0600

    update json tests

--- @phosphor/coreutils/test/json.spec.ts
+++ @phosphor/coreutils/test/json.spec.ts
@@ -10,7 +10,7 @@ import {
 } from 'chai';
 
 import {
-  JSONExt
+  JSONArray, JSONExt, JSONObject, JSONPrimitive
 } from '@phosphor/coreutils';
 
 
@@ -76,26 +76,38 @@ describe('@phosphor/coreutils', () => {
 
     });
 
-    describe('deepArrayEqual()', () => {
+    describe('deepCopy()', () => {
 
-      it('should compare two JSON arrays for deep equality', () => {
-        expect(JSONExt.deepEqual([], [])).to.equal(true);
-        expect(JSONExt.deepEqual([1], [1])).to.equal(true);
-        expect(JSONExt.deepEqual([1], [1, 2])).to.equal(false);
-        expect(JSONExt.deepEqual({ a: [1, 2] }, { a: [1, 2] })).to.equal(true);
-        expect(JSONExt.deepEqual({ a: [1, 2] }, { a: [1] })).to.equal(false);
-      });
-
-    });
-
-    describe('deepObjectEqual()', () => {
-
-      it('should compare two JSON objects for deep equality', () => {
-        expect(JSONExt.deepEqual({}, {})).to.equal(true);
-        expect(JSONExt.deepEqual({a: []}, {a: []})).to.equal(true);
-        expect(JSONExt.deepEqual({a: { b: null }}, {a: { b: null }})).to.equal(true);
-        expect(JSONExt.deepEqual({a: '1'}, {a: '1'})).to.equal(true);
-        expect(JSONExt.deepEqual({a: { b: null }}, {a: { b: '1' }})).to.equal(false);
+      it('should deep copy an object', () => {
+        let v1: JSONPrimitive = null;
+        let v2: JSONPrimitive = true;
+        let v3: JSONPrimitive = false;
+        let v4: JSONPrimitive = 'foo';
+        let v5: JSONPrimitive = 42;
+        let v6: JSONArray = [1, 2, 3, [4, 5, 6], { a: 12, b: [4, 5] }, false];
+        let v7: JSONObject = { a: false, b: [null, [1, 2]], c: { a: 1 } };
+        let r1 = JSONExt.deepCopy(v1);
+        let r2 = JSONExt.deepCopy(v2);
+        let r3 = JSONExt.deepCopy(v3);
+        let r4 = JSONExt.deepCopy(v4);
+        let r5 = JSONExt.deepCopy(v5);
+        let r6 = JSONExt.deepCopy(v6);
+        let r7 = JSONExt.deepCopy(v7);
+        expect(v1).to.equal(r1);
+        expect(v2).to.equal(r2);
+        expect(v3).to.equal(r3);
+        expect(v4).to.equal(r4);
+        expect(v5).to.equal(r5);
+        expect(v6).to.deep.equal(r6);
+        expect(v7).to.deep.equal(r7);
+        expect(v6).to.not.equal(r6);
+        expect(v6[3]).to.not.equal(r6[3]);
+        expect(v6[4]).to.not.equal(r6[4]);
+        expect((v6[4] as JSONObject)['b']).to.not.equal((r6[4] as JSONObject)['b']);
+        expect(v7).to.not.equal(r7);
+        expect(v7['b']).to.not.equal(r7['b']);
+        expect((v7['b'] as JSONArray)[1]).to.not.equal((r7['b'] as JSONArray)[1]);
+        expect(v7['c']).to.not.equal(r7['c']);
       });
 
     });
