4616ae054a9a0bd2eb12ca1f5144a5cf5065b585
S. Chris Colbert <sccolbert@gmail.com>
2017-03-07 12:47:02 -0600

    add alignment support to stacked layout

--- packages/widgets/src/stackedpanel.ts
+++ packages/widgets/src/stackedpanel.ts
@@ -72,6 +72,18 @@ class StackedPanel extends Panel {
 export
 namespace StackedPanel {
   /**
+   * A type alias for a widget horizontal alignment.
+   */
+  export
+  type HorizontalAlignment = StackedLayout.HorizontalAlignment;
+
+  /**
+   * A type alias for a widget vertical alignment.
+   */
+  export
+  type VerticalAlignment = StackedLayout.VerticalAlignment;
+
+  /**
    * An options object for creating a stacked panel.
    */
   export
@@ -82,6 +94,86 @@ namespace StackedPanel {
      * The default is a new `StackedLayout`.
      */
     layout?: StackedLayout;
+  }
+
+  /**
+   * Get the horizontal alignment for the given widget.
+   *
+   * @param widget - The widget of interest.
+   *
+   * @returns The horizontal alignment for the widget.
+   *
+   * #### Notes
+   * If the layout width allocated to a widget is larger than its max
+   * width, the horizontal alignment controls how the widget is placed
+   * within the extra horizontal space.
+   *
+   * If the allocated width is less than the widget's max width, the
+   * horizontal alignment has no effect.
+   */
+  export
+  function getHorizontalAlignment(widget: Widget): HorizontalAlignment {
+    return StackedLayout.getHorizontalAlignment(widget);
+  }
+
+  /**
+   * Set the horizontal alignment for the given widget.
+   *
+   * @param widget - The widget of interest.
+   *
+   * @param value - The value for the alignment.
+   *
+   * #### Notes
+   * If the layout width allocated to a widget is larger than its max
+   * width, the horizontal alignment controls how the widget is placed
+   * within the extra horizontal space.
+   *
+   * If the allocated width is less than the widget's max width, the
+   * horizontal alignment has no effect.
+   */
+  export
+  function setHorizontalAlignment(widget: Widget, value: HorizontalAlignment): void {
+    StackedLayout.setHorizontalAlignment(widget, value);
+  }
+
+  /**
+   * Get the vertical alignment for the given widget.
+   *
+   * @param widget - The widget of interest.
+   *
+   * @returns The vertical alignment for the widget.
+   *
+   * #### Notes
+   * If the layout height allocated to a widget is larger than its max
+   * height, the vertical alignment controls how the widget is placed
+   * within the extra vertical space.
+   *
+   * If the allocated height is less than the widget's max height, the
+   * vertical alignment has no effect.
+   */
+  export
+  function getVerticalAlignment(widget: Widget): VerticalAlignment {
+    return StackedLayout.getVerticalAlignment(widget);
+  }
+
+  /**
+   * Set the vertical alignment for the given widget.
+   *
+   * @param widget - The widget of interest.
+   *
+   * @param value - The value for the alignment.
+   *
+   * #### Notes
+   * If the layout height allocated to a widget is larger than its max
+   * height, the vertical alignment controls how the widget is placed
+   * within the extra vertical space.
+   *
+   * If the allocated height is less than the widget's max height, the
+   * vertical alignment has no effect.
+   */
+  export
+  function setVerticalAlignment(widget: Widget, value: VerticalAlignment): void {
+    StackedLayout.setVerticalAlignment(widget, value);
   }
 }
 
