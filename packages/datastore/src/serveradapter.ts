11bf83868e5b91f5401518fdbf12c7a77ae26c0e
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-05-30 18:33:28 -0700

    Docstrings

--- packages/datastore/src/serveradapter.ts
+++ packages/datastore/src/serveradapter.ts
@@ -18,13 +18,15 @@ import {
 } from '@phosphor/messaging';
 
 
-/**
- *
- */
+  /**
+   * A message class for `'patch-history'` messages.
+   */
 export
 class PatchHistoryMessage extends Message {
   /**
+   * Construct a new patch history message.
    *
+   * @param history - The patch history
    */
   constructor(history: IPatchHistory) {
     super('patch-history');
@@ -32,19 +34,21 @@ class PatchHistoryMessage extends Message {
   }
 
   /**
-   *
+   * The patch history
    */
   readonly history: IPatchHistory;
 }
 
 
 /**
- *
+ * A message class for `'remote-patch'` messages.
  */
 export
 class RemotePatchMessage extends Message {
   /**
+   * Construct a new remote patch message.
    *
+   * @param patch - The patch object
    */
   constructor(patch: IPatch) {
     super('remote-patch');
@@ -52,46 +56,46 @@ class RemotePatchMessage extends Message {
   }
 
   /**
-   *
+   * The patch object.
    */
   readonly patch: IPatch;
 }
 
 
 /**
- *
+ * A patch object for the data store.
  */
 export
 interface IPatch {
   /**
-   *
+   * The patch id.
    */
   readonly patchId: string;
 
   /**
-   *
+   * The id of the data store that generated the patch.
    */
   readonly storeId: number;
 
   /**
-   *
+   * The content representing the patch.
    */
   readonly content: ReadonlyJSONObject;
 }
 
 
 /**
- *
+ * A patch history object for the data store.
  */
 export
 interface IPatchHistory {
   /**
-   *
+   * The checkpoint that serves as the base of the patches.
    */
   readonly checkpoint: ReadonlyJSONObject;
 
   /**
-   *
+   * The known patches since the last checkpoint.
    */
   readonly patches: ReadonlyArray<IPatch>;
 }
