2597a70b6197a554cc39ef88bb6c6c66c1491b6a
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-05-31 13:35:10 -0700

    f docstrings

--- packages/datastore/src/serveradapter.ts
+++ packages/datastore/src/serveradapter.ts
@@ -102,27 +102,40 @@ type PatchHistory = {
 
 
 /**
- *
+ * A server adaptor object.
  */
 export
 interface IServerAdapter {
   /**
+   * Create a new, unique store ID.
    *
+   * @returns {Promise<number>} A promise to the new store id.
    */
   createStoreId(): Promise<number>;
 
   /**
+   * Register a handler for messages from the server adaptor.
    *
+   * @param {number} storeId The store id of the patch handler.
+   * @param {IMessageHandler} handler The patch handler to register.
+   * @returns {IDisposable} Disposable to use to unregister the handler.
    */
   registerPatchHandler(storeId: number, handler: IMessageHandler): IDisposable;
 
   /**
+   * Broadcast a patch to all data stores.
    *
+   * @param {number} storeId The store id of the patch source.
+   * @param {ReadonlyJSONObject} content The patch content.
+   * @returns {string} The patch id of the broadcasted patch.
    */
   broadcastPatch(storeId: number, content: ReadonlyJSONObject): string;
 
   /**
+   * Fetch specific patches from history by their id.
    *
+   * @param {string[]} patchIds The patch ids to fetch.
+   * @returns {Promise<Patch[]>} A promise to the patches that are fetched.
    */
   fetchPatches(patchIds: string[]): Promise<Patch[]>;
 }
