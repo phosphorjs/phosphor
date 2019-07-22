11ad5fd26f99fcbba6af51810e51346183393938
Saul Shanabrook <s.shanabrook@gmail.com>
2019-07-22 12:29:17 -0400

    Revert "Add coverage support"
    6ef99a6d1088d100283e86bc26efcd3d83437217

--- tests/test-datastore/karma.conf.js
+++ tests/test-datastore/karma.conf.js
@@ -2,17 +2,13 @@ module.exports = function (config) {
   config.set({
     basePath: '.',
     frameworks: ['mocha'],
-    reporters: ['mocha', 'coverage-istanbul'],
+    reporters: ['mocha'],
     files: ['build/bundle.test.js'],
     port: 9876,
     colors: true,
     singleRun: true,
     browserNoActivityTimeout: 30000,
     failOnEmptyTestSuite: false,
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'text-summary' ],
-      fixWebpackSourcePaths: true,
-    },
     logLevel: config.LOG_INFO
   });
 };
--- tests/test-datastore/webpack.config.js
+++ tests/test-datastore/webpack.config.js
@@ -4,15 +4,5 @@ module.exports = {
   entry: './build/index.spec.js',
   output: {
     filename: './build/bundle.test.js'
-  },
-  module: {
-    rules: [
-      // instrument only testing sources with Istanbul
-      {
-        test: /\.js$/,
-        use: { loader: 'istanbul-instrumenter-loader' },
-        include: path.resolve('../../packages/datastore/')
-      }
-    ]
   }
 }
