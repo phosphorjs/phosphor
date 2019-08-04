99b7e07f2e825174f4cb1ce15670df2a3430deaa
Ian Rose <ian.r.rose@gmail.com>
2019-08-04 10:21:34 -0700

    Move change handlers to be class methods.

--- examples/example-datastore/src/widget.ts
+++ examples/example-datastore/src/widget.ts
@@ -53,7 +53,7 @@ export class CodeMirrorEditor extends Panel {
     // Set up the DOM structure
     this._editorWidget = new Widget();
     this._checkWidget = new Widget();
-    this._checkWidget.node.style.height = '24px';
+    this._checkWidget.node.style.height = `${this._toolbarHeight}px`;
     this._checkWidget.addClass('read-only-check');
     this._checkWidget.node.textContent = 'Read Only';
     this._check = document.createElement('input');
@@ -65,52 +65,21 @@ export class CodeMirrorEditor extends Panel {
     this.addWidget(this._editorWidget);
 
     // Listen to changes in the checkbox.
-    this._check.onchange = () => {
-      // If this was a remote change, we are done.
-      if (this._changeGuard) {
-        return;
-      }
-      // Update the readonly state
-      this._editor.setOption("readOnly", this._check.checked);
-      // Update the table to broadcast the change.
-      let editorTable = this._store.get(EDITOR_SCHEMA);
-      datastore.beginTransaction();
-      editorTable.update({
-        [record]: {
-          readOnly: this._check.checked
-        }
-      });
-      datastore.endTransaction();
-    };
+    this._check.onchange = this._onChecked.bind(this);
 
     // Create the editor instance.
     this._editor = CodeMirror(this._editorWidget.node, {
       value: initialValue,
       lineNumbers: true,
+      readOnly
     });
 
     // Listen for changes on the editor model.
-    CodeMirror.on(this._editor.getDoc(), 'beforeChange', (doc, change) => {
-      // If this was a remote change, we are done.
-      if (this._changeGuard) {
-        return;
-      }
-      let start = doc.indexFromPos(change.from);
-      let end = doc.indexFromPos(change.to);
-      let text = change.text.join('\n');
-      // If this was a local change, update the table.
-      datastore.beginTransaction();
-      editorTable.update({
-        [record]: {
-          text: {
-            index: start,
-            remove: end - start,
-            text: text,
-          }
-        }
-      });
-      datastore.endTransaction();
-    });
+    CodeMirror.on(
+      this._editor.getDoc(),
+      'beforeChange',
+      this._onEditorChange.bind(this)
+    );
 
     // Listen for changes on the datastore.
     datastore.changed.connect(this._onDatastoreChange, this);
@@ -122,7 +91,7 @@ export class CodeMirrorEditor extends Panel {
    */
   protected onResize(msg: Widget.ResizeMessage): void {
     if (msg.width >= 0 && msg.height >= 0) {
-      this._editor.setSize(msg.width, msg.height - 24);
+      this._editor.setSize(msg.width, msg.height - this._toolbarHeight);
     } else if (this.isVisible) {
       this._editor.setSize(null, null);
     }
@@ -134,6 +103,53 @@ export class CodeMirrorEditor extends Panel {
    */
   onAfterShow() {
     this._editor.refresh();
+  }
+
+  /**
+   * Handle a local check event.
+   */
+  private _onChecked(): void {
+    // If this was a remote change, we are done.
+    if (this._changeGuard) {
+      return;
+    }
+    // Update the readonly state
+    this._editor.setOption("readOnly", this._check.checked);
+    // Update the table to broadcast the change.
+    let editorTable = this._store.get(EDITOR_SCHEMA);
+    this._store.beginTransaction();
+    editorTable.update({
+      [this._record]: {
+        readOnly: this._check.checked
+      }
+    });
+    this._store.endTransaction();
+  }
+
+  /**
+   * Handle a local editor change.
+   */
+  private _onEditorChange(doc: CodeMirror.Doc, change: CodeMirror.EditorChange): void {
+    // If this was a remote change, we are done.
+    if (this._changeGuard) {
+      return;
+    }
+    let editorTable = this._store.get(EDITOR_SCHEMA);
+    let start = doc.indexFromPos(change.from);
+    let end = doc.indexFromPos(change.to);
+    let text = change.text.join('\n');
+    // If this was a local change, update the table.
+    this._store.beginTransaction();
+    editorTable.update({
+      [this._record]: {
+        text: {
+          index: start,
+          remove: end - start,
+          text: text,
+        }
+      }
+    });
+    this._store.endTransaction();
   }
 
   /**
@@ -153,33 +169,34 @@ export class CodeMirrorEditor extends Panel {
         // Convert the change data to codemirror range and inserted text.
         let from = doc.posFromIndex(tc.index);
         let to = doc.posFromIndex(tc.index + tc.removed.length);
-
         let replacement = tc.inserted;
+
+          // Apply the operation, setting the change guard so we can ignore
+          // the change signals from codemirror.
+          this._changeGuard = true;
+          doc.replaceRange(replacement, from, to, '+input');
+          this._changeGuard = false;
+        });
+      }
+
+      // If the readonly state has changed, update the check box, setting the
+      // change guard so we can ignore it in the onchange event.
+      if(c && c[this._record] && c[this._record].readOnly) {
         this._changeGuard = true;
-        doc.replaceRange(replacement, from, to, '+input');
+        let checkChange = c[this._record].readOnly as RegisterField.Change<boolean>;
+        this._check.checked = checkChange.current;
+        // Update the readonly state
+        this._editor.setOption("readOnly", this._check.checked);
         this._changeGuard = false;
-      });
-      // Apply the operation, setting the change guard so we can ignore
-      // the change signals from codemirror.
+      }
     }
 
-    // If the readonly state has changed, update the check box, setting the
-    // change guard so we can ignore it in the onchange event.
-    if(c && c[this._record] && c[this._record].readOnly) {
-      this._changeGuard = true;
-      let checkChange = c[this._record].readOnly as RegisterField.Change<boolean>;
-      this._check.checked = checkChange.current;
-      // Update the readonly state
-      this._editor.setOption("readOnly", this._check.checked);
-      this._changeGuard = false;
-    }
+    private _changeGuard: boolean = false;
+    private _check: HTMLInputElement;
+    private _checkWidget: Widget;
+    private _editor: CodeMirror.Editor;
+    private _editorWidget: Widget;
+    private _record: string;
+    private _store: Datastore;
+    private _toolbarHeight = 24;
   }
-
-  private _changeGuard: boolean = false;
-  private _check: HTMLInputElement;
-  private _checkWidget: Widget;
-  private _editor: CodeMirror.Editor;
-  private _editorWidget: Widget;
-  private _record: string;
-  private _store: Datastore;
-}
